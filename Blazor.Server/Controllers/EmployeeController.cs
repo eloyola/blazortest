5585e2eefd8e83f0acc97fd23d24fefe505f2d88
Enrique <Enrique.loyola@gmail.com>
2020-06-18 19:34:28 +0100

    refactoring

--- Blazor.Server/Controllers/EmployeeController.cs
+++ Blazor.Server/Controllers/EmployeeController.cs
@@ -1,11 +1,8 @@
-﻿using Beam.Shared;
+﻿using Beam.Shared.Model;
 using Blazor.Data.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
-using Shared = Beam.Shared;
-using Blazor.Server.Mapper;
-using Beam.Shared.Model;
 
 namespace Blazor.Server.Controllers
 {
